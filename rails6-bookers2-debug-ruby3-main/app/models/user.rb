7c1f2fd62e5dd2e6df4ec2f42ecce0543424c4bf
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2024-12-29 13:31:07 +0000

    [add]follow

--- rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :followers, class_name: "relationships", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "relationships", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :follower_user, through: :followers, source: :follower
+  has_many :followed_user, through: :followeds, source: :followed
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
