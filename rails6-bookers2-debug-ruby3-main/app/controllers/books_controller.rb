cbd16d12bee4e1a69c32425b4ab0fd2adf689606
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2024-12-29 01:37:36 +0000

    [Finish]3

--- rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -3,7 +3,6 @@ class BooksController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
-    @book_new = Book.new
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
   end
